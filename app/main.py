9bde9f1aacf892da43178475a65ead221322360d
BrittonHelfert <brittonhelfert@gmail.com>
2026-04-06 14:06:30 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,14 +1,22 @@
 import sys
 
+COMMANDS = {
+    "exit": lambda line: sys.exit(0),
+    "echo": lambda line: print(line[5:]),
+    "type": lambda line: (
+        print(f"{line[5:]} is a shell builtin")
+        if line[5:] in COMMANDS
+        else print(f"{line[5:]}: not found")
+    ),
+}
+
 
 def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
-        if command == "exit":
-            break
-        if command.split(" ")[0] == "echo":
-            print(command[5:])
+        if command.split(" ")[0] in COMMANDS:
+            COMMANDS[command.split(" ")[0]](command)
         else:
             print(f"{command}: command not found")
 
