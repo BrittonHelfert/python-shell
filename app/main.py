42728214c5da0ccc9fc9924ca5e1518804fd588a
BrittonHelfert <brittonhelfert@gmail.com>
2026-04-08 16:24:48 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,14 +1,30 @@
+import os
 import sys
 
 COMMANDS = {
     "exit": lambda line: sys.exit(0),
     "echo": lambda line: print(line[5:]),
-    "type": lambda line: (
-        print(f"{line[5:]} is a shell builtin")
-        if line[5:] in COMMANDS
-        else print(f"{line[5:]}: not found")
-    ),
+    "type": lambda line: print(check_type(line[5:])),
 }
+
+
+def get_path(command: str) -> str | None:
+    if os.getenv("PATH") is not None:
+        for path in os.getenv("PATH").split(os.pathsep):
+            if os.path.exists(os.path.join(path, command)):
+                if os.access(os.path.join(path, command), os.X_OK):
+                    return os.path.join(path, command)
+    return None
+
+
+def check_type(command: str) -> str:
+    if command in COMMANDS:
+        return f"{command} is a shell builtin"
+    else:
+        path = get_path(command)
+        if path is not None:
+            return f"{command} is path"
+    return f"{command}: not found"
 
 
 def main():
