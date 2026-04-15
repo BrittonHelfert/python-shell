a3963aeb00f8d44d6320d6d72a0dee39f7fae641
BrittonHelfert <brittonhelfert@gmail.com>
2026-04-14 17:04:27 -0700

    codecrafters submit [skip ci]

--- app/parser.py
+++ app/parser.py
@@ -12,7 +12,7 @@ def parse_input(line: str) -> ParsedCommand:
 
 def split_input(line: str) -> list[str]:
     name = line.split(" ", 1)[0]
-    return [name, *extract_quotes(line[len(name) + 1 :])]
+    return [name, *extract_quotes(line[len(name) + 2 :])]
 
 
 def extract_quotes(line: str) -> list[str]:
