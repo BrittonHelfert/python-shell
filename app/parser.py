21321eae59cb7cce7376c2708e6b238f58ee23a9
BrittonHelfert <brittonhelfert@gmail.com>
2026-04-15 15:35:15 -0700

    codecrafters submit [skip ci]

--- app/parser.py
+++ app/parser.py
@@ -6,34 +6,31 @@ class ParseSyntaxError(Exception):
 
 
 def parse_input(line: str) -> ParsedCommand:
-    parts = split_input(line)
+    if line.strip() == "":
+        return ParsedCommand(name="", args=[])
+    parts = split_tokens(line)
     return ParsedCommand(name=parts[0], args=parts[1:])
 
 
-def split_input(line: str) -> list[str]:
-    name = line.split(" ", 1)[0]
-    return [name, *extract_quotes(line[len(name) + 1 :])]
-
-
-def extract_quotes(line: str) -> list[str]:
-    if line.count("'") % 2 == 0:
-        # Split on spaces until we reach a single quote, then add until the next single quote
-        res = []
-        start = 0
-        inside_single_quotes = False
-        while line[0] == " ":
-            line = line[1:]
-        for i, char in enumerate(line):
-            if char == "'":
-                res.append(line[start:i])
-                start = i + 1
-                inside_single_quotes = not inside_single_quotes
-            # Collapse spaces outside single quotes
-            elif char == " " and not inside_single_quotes:
-                while line[i + 1] == " ":
-                    line = line[: i + 1] + line[i + 2 :]
-        if start < len(line):
-            res.append(line[start:])
-        return res
-    else:
+def split_tokens(line: str) -> list[str]:
+    # Treat quoted strings as one token, otherwise split on spaces
+    res = []
+    curr_token = ""
+    inside_single_quotes = False
+    for char in line:
+        if char == "'":
+            inside_single_quotes = not inside_single_quotes
+        elif char == " ":
+            if inside_single_quotes:
+                curr_token += char
+            else:
+                if curr_token:
+                    res.append(curr_token)
+                    curr_token = ""
+        else:
+            curr_token += char
+    if curr_token:
+        res.append(curr_token)
+    if inside_single_quotes:
         raise ParseSyntaxError("unterminated single quote")
+    return res
