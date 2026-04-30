b5b6cb6b40d18b6b1a97612ba11477ac87be115f
BrittonHelfert <brittonhelfert@gmail.com>
2026-04-30 12:48:15 -0700

    codecrafters submit [skip ci]

--- app/builtins.py
+++ app/builtins.py
@@ -3,7 +3,7 @@ import shutil
 import sys
 from typing import Callable
 
-from .history import add_entries, get_history
+from .history import history
 from .jobs import list_jobs
 
 BUILT_IN_COMMANDS: dict[str, Callable[[list[str]], None]] = {
@@ -32,51 +32,3 @@ def check_type(command: str) -> str:
         if path is not None:
             return f"{command} is {path}"
     return f"{command}: not found"
-
-
-def history(args) -> None:
-
-    first_arg = args[0] if args else None
-
-    if first_arg == "-r":
-        try:
-            _read_history(args[1])
-        except IndexError:
-            raise ValueError("history: too few arguments")
-        return
-    elif first_arg == "-w" or first_arg == "-a":
-        try:
-            _write_history(first_arg[1:], args[1])
-        except IndexError:
-            raise ValueError("history: too few arguments")
-        return
-    else:
-        history = get_history()
-        if args:
-            try:
-                k_most_recent = int(args[0])
-            except ValueError:
-                raise ValueError("history: invalid argument")
-        else:
-            k_most_recent = len(history)
-        _print_history(history, k_most_recent)
-
-
-def _read_history(path: str) -> None:
-    with open(path, "r") as f:
-        history = f.read().splitlines()
-        add_entries(history)
-    return
-
-
-def _write_history(mode: str, path: str) -> None:
-    with open(path, mode) as f:
-        for entry in get_history():
-            f.write(entry + "\n")
-
-
-def _print_history(entries: list[str], k_most_recent: int) -> None:
-    for i, entry in enumerate(entries):
-        if i < len(entries) - k_most_recent:
-            continue
-        print(f"  {i + 1}  {entry}")
--- app/history.py
+++ app/history.py
@@ -1,13 +1,71 @@
 command_history: list[str] = []
+unstaged: list[str] = []
 
 
 def add_entry(entry: str) -> None:
     command_history.append(entry)
-
-
-def get_history() -> list[str]:
-    return command_history
+    unstaged.append(entry)
 
 
 def add_entries(entries: list[str]) -> None:
     command_history.extend(entries)
+
+
+def history(args) -> None:
+
+    first_arg = args[0] if args else None
+
+    if first_arg == "-r":
+        try:
+            _read_history(args[1])
+        except IndexError:
+            raise ValueError("history: too few arguments")
+        return
+    elif first_arg == "-w":
+        try:
+            _write_history(args[1])
+        except IndexError:
+            raise ValueError("history: too few arguments")
+        return
+    elif first_arg == "-a":
+        try:
+            _append_history(args[1])
+        except IndexError:
+            raise ValueError("history: too few arguments")
+        return
+    else:
+        if args:
+            try:
+                k_most_recent = int(args[0])
+            except ValueError:
+                raise ValueError("history: invalid argument")
+        else:
+            k_most_recent = len(command_history)
+        _print_history(command_history, k_most_recent)
+
+
+def _read_history(path: str) -> None:
+    with open(path, "r") as f:
+        history = f.read().splitlines()
+        add_entries(history)
+    return
+
+
+def _write_history(path: str) -> None:
+    with open(path, "w") as f:
+        for entry in command_history:
+            f.write(entry + "\n")
+
+
+def _append_history(path: str) -> None:
+    with open(path, "a") as f:
+        for entry in unstaged:
+            f.write(entry + "\n")
+    unstaged.clear()
+
+
+def _print_history(entries: list[str], k_most_recent: int) -> None:
+    for i, entry in enumerate(entries):
+        if i < len(entries) - k_most_recent:
+            continue
+        print(f"  {i + 1}  {entry}")
